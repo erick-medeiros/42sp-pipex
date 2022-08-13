7b773a6d973e9900c9a3ac3f5ab6309b7be64d6b
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-08-13 11:10:42 -0300

    refactor: concat paths with "/"

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 15:08:22 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/08/12 23:19:58 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/08/13 10:58:53 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,11 @@ static int	pipex_open(char *pathname, int mode)
 
 static char	*get_runpath(char **paths, char *cmd_exec)
 {
-	char	*tmp;
 	char	*runpath;
 
 	while (*paths)
 	{
-		tmp = ft_strjoin(*paths, "/");
-		runpath = ft_strjoin(tmp, cmd_exec);
-		free(tmp);
+		runpath = ft_strjoin(*paths, cmd_exec);
 		if (access(runpath, F_OK | X_OK) == 0)
 			return (runpath);
 		free(runpath);
@@ -49,12 +46,20 @@ static char	*get_runpath(char **paths, char *cmd_exec)
 
 void	pipex_init(t_pipex *pipex, int argc, char **argv, char **envp)
 {
+	size_t	i;
+
 	pipex->envp = envp;
 	pipex->paths = NULL;
 	while (*envp != NULL && ft_strncmp("PATH", *envp, 4))
 		envp++;
 	if (envp != NULL)
 		pipex->paths = ft_split(*envp + 5, ':');
+	if (pipex->paths != NULL)
+	{
+		i = -1;
+		while (pipex->paths[++i] != NULL)
+			ft_strupd(&pipex->paths[i], ft_strjoin(pipex->paths[i], "/"));
+	}
 	pipex->infile = pipex_open(argv[1], IN_MODE);
 	if (pipex->infile < 0)
 		error(ERR_INFILE, NULL);
